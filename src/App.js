daa6d03e29110dcf676b65faa67962dc279b823a
Ashley Davis <ashley@codecapers.com.au>
2020-07-29 15:27:01 +1000

    Integrated data and rendering a table from it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './app.css';
+import * as characters from "./data/characters.json";
 
 function App() {
+    
+    const columns = Object.keys(characters.default[0]);
+    const data = characters.default;
+
     return (
         <div>
-            Hello world!
+            <table>
+                <thead>
+                    <tr>
+                        {columns.map(columnName => 
+                            <th key={columnName}>
+                                {columnName}
+                            </th>
+                        )}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(record => 
+                        <tr key={record.name}>
+                            {columns.map(columnName => 
+                                <td key={`${record.name}-${columnName}`}>
+                                    {record[columnName]}
+                                </td>
+                            )}
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+
+            <pre>
+                {JSON.stringify(characters, null, 4)}
+            </pre>
         </div>
     );
 }
