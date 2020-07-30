070382191dd41f2b379d19422857e5ce71e26fcf
Ashley Davis <ashley@codecapers.com.au>
2020-07-30 19:17:09 +1000

    Setting home page to try and get relative paths working in GitHub pages.

---
+++ docs/precache-manifest.258e58bb951e34b53ee79308a741064e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9517a93e6c36efd12ebb4b7b01c7897a",
+    "url": "/miniql-interactive-example/index.html"
+  },
+  {
+    "revision": "6c1092fdaf70f558fb4f",
+    "url": "/miniql-interactive-example/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "7a7d3af289814271b8cc",
+    "url": "/miniql-interactive-example/static/css/main.5aa8cc0d.chunk.css"
+  },
+  {
+    "revision": "6c1092fdaf70f558fb4f",
+    "url": "/miniql-interactive-example/static/js/2.9c912416.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/miniql-interactive-example/static/js/2.9c912416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7d3af289814271b8cc",
+    "url": "/miniql-interactive-example/static/js/main.218fa38c.chunk.js"
+  },
+  {
+    "revision": "d6617b85b812a494355a",
+    "url": "/miniql-interactive-example/static/js/runtime-main.d52fb8cd.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.658eed22d34a697679f179e59b1691f1.js"
+  "/miniql-interactive-example/precache-manifest.258e58bb951e34b53ee79308a741064e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/miniql-interactive-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
