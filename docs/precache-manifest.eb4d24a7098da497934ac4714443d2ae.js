219cdb1a669de1b4858be744982f15fa20312654
Ashley Davis <ashley@codecapers.com.au>
2020-07-30 16:44:12 +1000

    Built the interactive example to a static web site.

---
+++ docs/precache-manifest.eb4d24a7098da497934ac4714443d2ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3463b06069680cfaabf6b47f89fa5ad1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c1092fdaf70f558fb4f",
+    "url": "/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "7a7d3af289814271b8cc",
+    "url": "/static/css/main.5aa8cc0d.chunk.css"
+  },
+  {
+    "revision": "6c1092fdaf70f558fb4f",
+    "url": "/static/js/2.9c912416.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9c912416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7d3af289814271b8cc",
+    "url": "/static/js/main.218fa38c.chunk.js"
+  },
+  {
+    "revision": "49b993fa745889393a3e",
+    "url": "/static/js/runtime-main.1f1423fe.js"
+  }
+]);
